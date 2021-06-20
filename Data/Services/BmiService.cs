2443503cf8f2fcc4b2958237be551212e439ab0e
Alex Rogers <alex.rogers823@gmail.com>
2021-06-20 17:31:01 -0500

    set up files for bmi controller

---
+++ Data/Services/BmiService.cs
@@ -0,0 +1,27 @@
+using System.Collections.Generic;
+
+namespace FlourishFitness.Pages
+{
+    public class BmiService : IBmi
+    {
+        public void AddBmi()
+        {
+            throw new System.NotImplementedException();
+        }
+
+        public IEnumerable<Bmi> GetBmiHistory()
+        {
+            throw new System.NotImplementedException();
+        }
+
+        public Bmi GetCurrentBmi()
+        {
+            throw new System.NotImplementedException();
+        }
+
+        public void UpdateBmi()
+        {
+            throw new System.NotImplementedException();
+        }
+    }
+}
